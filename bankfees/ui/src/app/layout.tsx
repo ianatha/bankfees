ce82567416a4b3f529988640388e0f43356d22f5
Ian Atha <ian@atha.io>
2025-06-14 16:09:10 +0300

    fonts

--- bankfees/ui/src/app/layout.tsx
+++ bankfees/ui/src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Mono, Fira_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const fontSans = Fira_Sans({
+  variable: "--font-fira-sans",
+  subsets: ["latin", "greek"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const fontMono = Fira_Mono({
+  variable: "--font-fira-mono",
+  subsets: ["latin", "greek"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontSans.variable} ${fontMono.variable} antialiased font-sans`}
       >
         {children}
       </body>
